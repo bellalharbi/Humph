88cbe26c1c8b73b3b43eaa1c3dbed8b9a708418c
bellalharbi <balqeesalharbi@gmail.com>
2016-07-29 23:42:48 -0700

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,17 +1,17 @@
 var pageData = collectData();
-var url = document.querySelector('meta[property=og:url]').getAttribute('content');
-var title = document.querySelector('meta[property=og:title]').getAttribute('content');
-var source = document.querySelector('meta[property=og:site_name]').getAttribute('content');
-var date = document.querySelector('meta[name=date]').getAttribute('content') || document.querySelector('meta[property=article:published]').getAttribute('content') || document.querySelector('meta[property=article:published_time]').getAttribute('content');
-var description = document.querySelector('meta[property=og:description]').getAttribute('content');
-var keywords = document.querySelector('meta[name=keywords]').getAttribute('content') || document.querySelector('meta[name=news_keywords]').getAttribute('content');
-var imageurl = document.querySelector('meta[name=twitter:image]').getAttribute('content');
+var urlX = document.querySelector('meta[property=og:url]').getAttribute('content');
+var titleX = document.querySelector('meta[property=og:title]').getAttribute('content');
+var sourceX = document.querySelector('meta[property=og:site_name]').getAttribute('content');
+var dateX = document.querySelector('meta[name=date]').getAttribute('content') || document.querySelector('meta[property=article:published]').getAttribute('content') || document.querySelector('meta[property=article:published_time]').getAttribute('content');
+var descriptionX = document.querySelector('meta[property=og:description]').getAttribute('content');
+var keywordsX = document.querySelector('meta[name=keywords]').getAttribute('content') || document.querySelector('meta[name=news_keywords]').getAttribute('content');
+var imageurlX = document.querySelector('meta[name=twitter:image]').getAttribute('content');
 
 
 function collectData(){
     pageData = [url, title, source, date, description, keywords, imageurl];
 }
 
-chrome.runtime.sendMessage(pageData, function(response){
+chrome.runtime.sendMessage({url: urlX, title: titleX, source: sourceX, date: dateX, description: descriptionX, keywords: keywordsX, imageurl: imageurlX} , function(response){
     console.log(response);
 })
