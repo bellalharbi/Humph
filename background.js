3a8c44fb4cb3a642315da9588b1ad0b612e5c814
bellalharbi <balqeesalharbi@gmail.com>
2016-07-30 00:37:49 -0700

    Update background.js

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onMessage.addListener(
         console.log(sender.tab.url);
         var info = 'url='+request.url+'&title='+request.title+'&source='+request.source+'&date='+request.date+'&description='+request.description+'&keywords='+request.keywords+'&imageurl='+request.imageurl;
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://bellalharbi.com/humph.php", true);
+        xhr.open("POST", "http://bellalharbi.com/forbell/humph.php", true);
         xhr.onreadystatechange = function() {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 console.log(xhr.responseText);
