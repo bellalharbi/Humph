959668baff32cbf27c6fd4ff528a88053b3a93ef
bellalharbi <balqeesalharbi@gmail.com>
2016-07-29 22:38:09 -0700

    keywords

--- scrape.js
+++ scrape.js
@@ -4,7 +4,7 @@ var title = document.querySelector('meta[property=og:title]').getAttribute('cont
 var source = document.querySelector('meta[property=og:site_name]').getAttribute('content');
 var date = document.querySelector('meta[name=date]').getAttribute('content') || document.querySelector('meta[property=article:published]').getAttribute('content') || document.querySelector('meta[property=article:published_time]').getAttribute('content');
 var description = document.querySelector('meta[property=og:description]').getAttribute('content');
-var keywords = document.querySelector('meta[name=keywords]').getAttribute('content');
+var keywords = document.querySelector('meta[name=keywords]').getAttribute('content') || document.querySelector('meta[name=news_keywords]').getAttribute('content');
 var imageurl = document.querySelector('meta[name=twitter:image]').getAttribute('content');
 
 
