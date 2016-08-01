8e2a6f1a2778dec258ef4752e6afa4fdea86e0d7
bellalharbi <balqeesalharbi@gmail.com>
2016-07-31 19:58:30 -0700

    remove onMessage from background.js

--- background.js
+++ background.js
@@ -1,19 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file: "content_script.js"});
 });
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log(sender.tab.url);
-        var info = 'url='+request.url+'&title='+request.title+'&source='+request.source+'&date='+request.date+'&description='+request.description+'&keywords='+request.keywords+'&imageurl='+request.imageurl;
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://bellalharbi.com/forbell/humph.php", true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                console.log(xhr.responseText);
-            }
-        };
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.send(info);
-        sendResponse({status: "received"});
-    });
