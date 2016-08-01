8d3728b25aa5063526e0965f23a6464561ae0a2a
bellalharbi <balqeesalharbi@gmail.com>
2016-07-31 20:19:01 -0700

    cross fingers for scrape.js

--- scrape.js
+++ scrape.js
@@ -6,6 +6,8 @@ var descriptionX = document.querySelector('meta[property=og:description]').getAt
 var keywordsX = document.querySelector('meta[name=keywords]').getAttribute('content') || document.querySelector('meta[name=news_keywords]').getAttribute('content') || "Not found";
 var imageurlX = document.querySelector('meta[name=twitter:image]').getAttribute('content') || "Not found";
 
+//need to run a check to see if url already in table and, if so, alert and stop script here
+
 var modalCss = "#humphModal {
     display: none;
     position: fixed;
@@ -13,8 +15,9 @@ var modalCss = "#humphModal {
     width:100%;
     height:100%;
     top:0;
-    left:0; }
-#humphContent {
+    left:0; 
+    background-color: rgba(0,0,0,0.4); 
+} #humphContent {
     display: none;
     position: fixed;
     z-index: 101;
@@ -24,23 +27,39 @@ var modalCss = "#humphModal {
     max-height:700px;
     top: 200px;
     left: 25%;
-    background-color: rgba(0,0,0,0.4); }
-.humph-unit {
-    
-}
-.humph-input {
-    
-}
-.humph-description {
-    
-}
-.humph-close-modal {
-    
-}
-.humph-submit-button {
-    
-}
-    "
+    background-color: #FFFFFF; 
+    font-size: 18px;
+} #humphHeader {
+    font-size: 26px; 
+    width:100%;
+    background-color: #5A3B4D;
+    height:50px;
+    line-height: 30px;
+    color: #FFFFFF;
+} .humph-unit {
+    width: 100%;
+    margin: 5px;
+    height: 70px;
+} .humph-input {
+    height: 30px;
+    border: 2px solid #C4C4C4;
+    border-radius: 2px;
+    background-color: #F2F2F2;
+    width: 90%;
+    font-size: 18px;
+} .humph-text-area {
+    //do later UGH THIS CSS IS SO BORING, I CAN'T EVEN TEST IT i probably can i'm just dumb and lazy, a very powerful combination
+} #humphClose {
+    position: relative;
+    float: right;
+    margin: 5px;
+} #humphSubmit {
+    background-color: #5A3B4D;
+    font-size: 24px;
+    color: #FFFFFF;
+    width: 80px;
+    height: 40px;
+}"
     
 var humphStyle = document.createElement('style');
 humphStyle.type = 'text/css';
@@ -62,22 +81,69 @@ while (indx > 0){
 if (indArray.length > 0){
     //modal where I can manually insert values for the missing variables
     document.createElement("div").setAttribute("id", "humphModal");
-    document.createElement("div").setAttribute("id", "humphContent");
+    var initHumphContent = document.createElement("div").setAttribute("id", "humphContent");
+    var humphForm = document.createElement("form").setAttribute("action", "http://bellalharbi.com/forbell/humph.php").setAttribute("method", "post");
     var humphContent = document.getElementById("humphContent");
+    humphForm.appendChild(initHumphContent);
+    var humphUnitVal, humphInputVal, humphInputName, humphInputType;
+    var humphModalUnit = document.createElement("div").setAttribute("class", "humph-unit").innerHTML = humphUnitVal;
+    var humphInput = document.createElement("input").setAttribute("class", "humph-input").setAttribute("value", humphInputVal).setAttribute("name", humphInputName).setAttribute("type", humphInputType);
+    var initHumphHeader = document.createElement("div").setAttribute("id", "humphHeader");
+    humphContent.appendChild(initHumphHeader);
+    var humphHeader = document.getElementById("humphHeader")
+    humphHeader.innerHTML = 'Missing '+indArray.length+' fields <div id="humphClose">x</div>';
     //fill modal content
-    //create loop for i = [1, 7]
-    //for each i, a humph-unit and humph-input
-    //humph-input values = ___X vars, if "Not found", value = ""
-    var humphModalUnit = document.createElement("div").setAttribute("class", "humph-unit");
-    var humphInput = document.createElement("input").setAttribute("class", "humph-input");
-    humphContent.appendChild(humphModalUnit);
-    humphContent.appendChild(humphInput); //define input name for each value of i
-    
-    //on submit, post data to humph.php
-    
+    for (var i = 0, i < 7, i++){
+        if (i=0){
+            humphUnitVal = "URL";
+            humphInputVal = urlX;
+            humphInputName = "url";
+            humphInputType = "url";
+        } else if (i=1){
+            humphUnitVal = "Title";
+            humphInputVal = titleX;
+            humphInputName = "title";
+            humphInputType = "text";
+        } else if (i=2){
+            humphUnitVal = "Source";
+            humphInputVal = sourceX;
+            humphInputName = "source";
+            humphInputType = "text";
+        } else if (i=3){
+            humphUnitVal = "Publish Date";
+            humphInputVal = dateX;
+            humphInputName = "date";
+            humphInputType = "date";
+        } else if (i=4){
+            humphUnitVal = "Description";
+            humphInputVal = descriptionX;
+            humphInputName = "description";
+            humphInputType = "text";
+        } else if (i=5){
+            humphUnitVal = "Keywords";
+            humphInputVal = keywordsX;
+            humphInputName = "keywords";
+            humphInputType = "text";
+        } else if (i=6){
+            humphUnitVal = "Image Url";
+            humphInputVal = imageurlX;
+            humphInputName = "imageurl";
+            humphInputType = "url";
+        }
+        humphContent.appendChild(humphModalUnit);
+        humphContent.appendChild(humphInput); 
+    }
+    var humphSubmit = document.createElement("input").setAttribute("type", "submit").setAttribute("value", "Submit").setAttribute("id", "humphSubmit");
+    humphContent.appendChild(humphSubmit);
 } else {
-    //fuck, do i even need to send message??? I should xhr and post directly to humph.php
-    chrome.runtime.sendMessage({url: urlX, title: titleX, source: sourceX, date: dateX, description: descriptionX, keywords: keywordsX, imageurl: imageurlX} , function(response){
-        console.log(response);
-    });
+    var info = 'url='+urlX+'&title='+titleX+'&source='+sourceX+'&date='+dateX+'&description='+descriptionX+'&keywords='+keywordsX+'&imageurl='+imageurlX;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://bellalharbi.com/forbell/humph.php", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(info);
 } 
